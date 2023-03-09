9052d1679da5e3f1f966e4d053042aeead1e43c5
TANGHULU6 <12110502@mail.sustech.edu.cn>
2023-03-09 17:50:27 +0800

    de

--- src/quiz1.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-
-public class quiz1 {
-    public static void main(String[] args) {
-        int sum= Arrays.stream(new int[]{1,2,3}).filter(i->i>=2).map(i->i*3).sum();System.out.println(sum);
-    }
-}
