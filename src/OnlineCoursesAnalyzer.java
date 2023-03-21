be5c68395989f1cbfc2463348db966d43833b05f
TANGHULU6 <12110502@mail.sustech.edu.cn>
2023-03-21 17:10:41 +0800

    修改格式前

--- src/OnlineCoursesAnalyzer.java
+++ src/OnlineCoursesAnalyzer.java
@@ -15,7 +15,20 @@ public class OnlineCoursesAnalyzer {
         private double $Audited,$Certified,$Certified_of_50_CourseBF_Content_Accessed,$Played_Video,$Posted_in_Forum,$Grade_Higher_Than_Zero ,Total_CourseBF_Hours,Median_Hours_for_Certification,$Male,$Female,$Bachelor_Degree_or_Higher;
         private int Median_Age;
         private String pair;
+        private String[] InstructorArray;
+        private double similarity;
 
+        public double getSimilarity() {
+            return similarity;
+        }
+
+        public void setSimilarity(double similarity) {
+            this.similarity = similarity;
+        }
+
+        public String[] getInstructorArray() {
+            return InstructorArray;
+        }
 
         public String getInstitution() {
             return Institution;
@@ -113,8 +126,9 @@ public class OnlineCoursesAnalyzer {
             try{
                 Institution = institution;
                 CourseBF_Number = courseBF_Number;
-                CourseBF_Title = courseBF_Title;
+                CourseBF_Title = courseBF_Title.replaceAll("\"","");
                 Instructors = instructors.replaceAll("\"","");
+                InstructorArray=Instructors.split(", ");
                 CourseBF_Subject = courseBF_Subject.replaceAll("\"","");
                 pair=Institution+"-"+CourseBF_Subject;
                 SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
@@ -176,7 +190,7 @@ public class OnlineCoursesAnalyzer {
         Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
                 if(o2.getValue().compareTo(o1.getValue())==0){
-                    return o2.getKey().compareTo(o1.getKey());
+                    return o1.getKey().compareTo(o2.getKey());
                 }
                 return  o2.getValue().compareTo(o1.getValue());
             }
@@ -187,32 +201,143 @@ public class OnlineCoursesAnalyzer {
     }
 
     public Map<String, List<List<String>>> getCourseListOfInstructor(){
-        Stack<List> stack=new Stack<>();
-        List<course> answer= getCourses().toList();
-        for (course cd:
-             answer) {
-            answer.stream().map(n->{
-                stack.push(Arrays.asList(n.getInstructors().split(",")));
-                return n;
-            }).collect(
-                    Collectors.toMap(stack.pop().stream().limit(1),,(v1, v2)->v1,LinkedHashMap::new)
+        Map<String, List<List<String>>> answer = new HashMap<>();
+        Map<String, List<String>> answer3= getCourses().collect(
+                  Collectors.groupingBy(course::getInstructors,LinkedHashMap::new,Collectors.mapping(course::getCourseBF_Title,Collectors.toList()))
             );
-        }
 
-        return answer3;
+        answer3.forEach((k,v)->
+                {   String[] dd=k.split(", ");
+                    if(dd.length==1){
+                        List<List<String>> List=new LinkedList<>();
+                        List<String> List0 = new ArrayList<>();
+                        List<String> List1 = new ArrayList<>();
+                        List.add(List0);List.add(List1);
+                        List0.addAll(v);
+                        answer.merge(dd[0],List, (o,n)->{
+                            n.get(0).addAll(o.get(0));
+                            n.get(1).addAll(o.get(1));
+                            Collections.sort(n.get(0));
+                            return n;
+                        });
+
+
+                    }else {
+                        for (String si :
+                                dd) {
+                            List<List<String>> List=new LinkedList<>();
+                            List<String> List0 = new ArrayList<>();
+                            List<String> List1 = new ArrayList<>();
+                            List.add(List0);List.add(List1);
+                            List1.addAll(v);
+                            answer.merge(si,List, (o,n)->{
+                                n.get(0).addAll(o.get(0));
+                                n.get(1).addAll(o.get(1));
+                                Collections.sort(n.get(0));Collections.sort(n.get(1));
+                                return n;
+                            });
+
+                        }
+
+                    }
+                }
+        );
+
+        answer.forEach(
+                (k,v)->{
+                    List new0=v.get(0).stream().distinct().collect(Collectors.toList());
+                    v.get(0).clear();v.get(0).addAll(new0);
+                    List new1=v.get(1).stream().distinct().collect(Collectors.toList());
+                    v.get(1).clear();v.get(1).addAll(new1);
+                    Collections.sort(v.get(0));Collections.sort(v.get(1));
+                }
+        );
+
+        return answer;
     }
     public List<String> getCourses(int topK, String by){
-        return null;
-
+        List<String> answer=null ;
+       switch (by){
+           case "hours":
+               answer=getCourses().sorted(Comparator.comparing(course::getTotal_CourseBF_Hours).reversed().thenComparing(course::getCourseBF_Title)).map(course::getCourseBF_Title).distinct().limit(topK).toList();
+               break;
+           case"participants":
+               answer=getCourses().sorted(Comparator.comparing(course::getParticipants_CourseBF_Content_Accessed).reversed().thenComparing(course::getCourseBF_Title)).map(course::getCourseBF_Title).distinct().limit(topK).toList();
+               break;
+           default:System.exit(-1);
+               break;
+       }
+    return answer;
     }
     public List<String> searchCourses(String courseSubject, double
             percentAudited, double totalCourseHours){
-        return null;
+        List<String> answer=null ;
+        answer=getCourses()
+                .filter(c->c.getCourseBF_Subject().toLowerCase().contains(courseSubject.toLowerCase()))
+                .filter(c->c.get$Audited()>=percentAudited)
+                .filter(c->c.getTotal_CourseBF_Hours()<=totalCourseHours)
+                .sorted(Comparator.comparing(course::getCourseBF_Title))
+                .map(course::getCourseBF_Title)
+                .distinct()
+                .toList();
+
+        return answer;
 
     }
     public List<String> recommendCourses(int age, int gender, int
             isBachelorOrHigher){
-        return null;
+        List<String> answer=new ArrayList<>() ;
+        Map<String, List<course>> temp = getCourses().collect(Collectors.groupingBy(course::getCourseBF_Number));
+//        Map temp_Age  =getCourses().collect(Collectors.groupingBy(course::getCourseBF_Number,Collectors.summingInt(course::getMedian_Age)));
+//        Map temp_cnt  =getCourses().collect(Collectors.groupingBy(course::getCourseBF_Number,Collectors.counting()));
+//        Map temp_Male  =getCourses().collect(Collectors.groupingBy(course::getCourseBF_Number,Collectors.summingDouble(course::get$Male)));
+//        Map temp_bachelor =getCourses().collect(Collectors.groupingBy(course::getCourseBF_Number,Collectors.summingDouble(course::get$Bachelor_Degree_or_Higher)));
+//        Map average_Age = getCourses().collect(Collectors.groupingBy(course::getCourseBF_Number))
+//                .values().stream().mapToInt(c-> {
+//                     Map<String, Double> dddd= c.stream().collect(Collectors.groupingBy(course::getCourseBF_Number, Collectors.averagingInt(course::getMedian_Age)));
+//                     return dddd.values().stream().mapToInt().sum();
+//
+//                }).average();
+        Map<String,Double> average_Age = getCourses().collect(Collectors.groupingBy(course::getCourseBF_Number,Collectors.averagingInt(course::getMedian_Age)));
+        Map<String,Double> average_Male  = getCourses().collect(Collectors.groupingBy(course::getCourseBF_Number,Collectors.averagingDouble(course::get$Male)));
+        Map<String,Double> average_bachelor = getCourses().collect(Collectors.groupingBy(course::getCourseBF_Number,Collectors.averagingDouble(course::get$Bachelor_Degree_or_Higher)));
+        Map<String,Double> similarity = new HashMap<>();
+        for (String key:
+                temp.keySet()) {
+            double ageT=average_Age.get(key);
+            double $maleT=average_Male.get(key);
+            double bachelorT=average_bachelor.get(key);
+            double sim=(age-ageT)*(age-ageT)+(gender*100-$maleT)*(gender*100-$maleT)+(isBachelorOrHigher*100-bachelorT)*(isBachelorOrHigher*100-bachelorT);
+            similarity.put(key,sim);
+            temp.get(key).forEach(co->co.setSimilarity(sim));
+        }
+//        List<String> course10Num=similarity.entrySet().stream().sorted(new Comparator<Map.Entry<String, Double>>() {
+//            @Override
+//            public int compare(Map.Entry<String, Double> o1, Map.Entry<String, Double> o2) {
+//                if(o2.getValue().compareTo(o1.getValue())==0){
+//                    return 0;
+//                }
+//                return  o1.getValue().compareTo(o2.getValue());
+//            }
+//        }).map(Map.Entry::getKey).toList();
+//        for (String key :
+//                course10Num) {
+//            answer.add(temp.get(key).stream().sorted(Comparator.comparing(course::getLaunch_Date).reversed()).toList().get(0).getCourseBF_Title());
+//            answer=answer.stream().distinct().collect(Collectors.toList());
+//            if(answer.size()==10){
+//                break;
+//            }
+//        }
+        answer=getCourses().sorted(Comparator.comparing(course::getSimilarity).thenComparing(course::getCourseBF_Title))
+
+                .map(n->{
+                    return temp.get(n.getCourseBF_Number()).stream().sorted(Comparator.comparing(course::getLaunch_Date).reversed()).toList().get(0);
+                })
+                .map(course::getCourseBF_Title)
+                .distinct()
+                .limit(10)
+                .toList();
+        return answer;
 
     }
 
